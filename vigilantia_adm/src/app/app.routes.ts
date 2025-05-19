d8b01dcc20e28e7714101f948129dc09cb765450
koikera <matheuscomelli@hotmail.com>
2025-05-19 19:37:37 -0300

    Criacao da tela de primeiro acesso

--- vigilantia_adm/src/app/app.routes.ts
+++ vigilantia_adm/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { FirstAccessComponent } from './components/first-access/first-access.component';
 
 export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'first-access',
+        component: FirstAccessComponent
     }
 ];
---
+++ vigilantia_adm/src/app/components/first-access/first-access.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-access',
+  imports: [],
+  templateUrl: './first-access.component.html',
+  styleUrl: './first-access.component.scss'
+})
+export class FirstAccessComponent {
+
+}
